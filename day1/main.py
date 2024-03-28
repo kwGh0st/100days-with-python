ea38cfc7d12f553eeaab3dfbd603e19f3c6f0734
Karol Programuje <karol341654@gmail.com>
2024-03-28 10:58:22 +0100

    band name generator

---
+++ day1/main.py
@@ -0,0 +1,5 @@
+print("Hello in band name generator program! \n")
+city = input("Enter city that you grow up. \n")
+pet_name = input("Enter your pet name. \n")
+
+print("Your band name is: " + city + " " + pet_name); 
